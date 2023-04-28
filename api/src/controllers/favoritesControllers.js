b33f80bab6c759baa8fedd6da54bd995343bea74
Mauricio Mendez <emauriciomendez@gmail.com>
2023-04-27 23:54:16 -0500

    adicionando getFavorites y controller

---
+++ api/src/controllers/favoritesControllers.js
@@ -0,0 +1,17 @@
+const getAllFavoritesController = async (req, res) => {
+    try {
+        console.log("Estoy en el controller de Favorites")
+        const favoriteFromDb = await Products.findAll({ where : { productFavorite : true }})
+        if(favoriteFromDb.length === 0) throw Error("No hay Favorits en la DB");
+        res.status(200).json(favoriteFromDb);
+    } catch (error) {
+        console.error('Error al mostrar los Users de la DB:', error);
+        res.status(400).json({ error: error.message });
+    }
+ }
+// const getFavorites = async () => {
+//     return await Products.findAll({ where : { productFavorite : true }}) // productFavorite seria la relación de la tabla de producto con favorito
+// }
+module.exports = {
+    getAllFavoritesController
+}
--- api/src/routes/favoritesRouter.js
+++ api/src/routes/favoritesRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const {getAllFavoritesController} =require('../controllers/favoritesControllers')
 
 // Requerir todos los handlers
 // ejemplo: const handlerFavorites = require('../handlers/handlerFavorites)
@@ -9,13 +10,14 @@ const { Router } = require('express');
 // ejemplo: const helperFavorites = require('../helpers/handlerFavorites)
 
 
-
+console.log('en ruta')
 
 const favoritesRouter = Router();
 
 // Especificar todas las peticiones
 // ejemplo: favoritesRouter.get('/favorites', helperFavorites, handlerFavorites)
 
+favoritesRouter.get('/', getAllFavoritesController)
 
 
 
