d093f1943926fcd24b5b035264e3dc9294011646
ajpintos <ajpintos@gmail.com>
2023-05-03 00:13:21 -0300

    se puso ruta alternativa de CORS comentada para usar en deploy y poder cambiar facilmente de una a otra

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    //res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); //! no borrar! este setting se usa para el
+    // deploy
+    // the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
