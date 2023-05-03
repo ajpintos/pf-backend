71a846e25aea4641d9220d1c1e99689f2ff67582
santimuller45 <santiagomuller45@gmail.com>
2023-05-02 22:43:36 -0300

    added imgs from backend

--- api/src/helpers/productsArray.js
+++ api/src/helpers/productsArray.js
@@ -1,133 +1,133 @@
 const productsArray = [
     {
         name: "Almendras",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/almendras.jpg",
         description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
         price: 2.50,
         stock: 50
     },
     {
         name: "Dátiles",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/datiles.jpg",
         description: "Fruta deshidratada rica en fibra y vitaminas.",
         price: 3.50,
         stock: 30
     },
     {
         name: "Ciruelas pasas",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/pasas.jpg",
         description: "Fruta deshidratada rica en fibra, antioxidantes y compuestos beneficiosos para la salud.",
         price: 4.00,
         stock: 20
     },
     {
         name: "Nueces de Brasil",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/nueces-brasil.jpg",
         description: "Fruto seco rico en grasas saludables, proteínas y minerales.",
         price: 6.00,
         stock: 40
     },
     {
         name: "Higos secos",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/higos-secos.jpg",
         description: "Fruta deshidratada rica en fibra, antioxidantes y compuestos beneficiosos para la salud.",
         price: 3.50,
         stock: 15
     },
     {
         name: "Pistachos",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/pistachos.jpg",
         description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
         price: 5.00,
         stock: 25
     },
     {
         name: "Anacardos",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/anacardos.jpg",
         description: "Fruto seco rico en grasas saludables, proteínas y minerales.",
         price: 7.50,
         stock: 35
     },
     {
         name: "Arándanos deshidratados",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/arandanos-desidratados.jpg",
         description: "Fruta deshidratada rica en antioxidantes y compuestos beneficiosos para la salud.",
         price: 4.25,
         stock: 10
     },
     {
         name: "Avellanas",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/avellanas.jpg",
         description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
         price: 2.25,
         stock: 60
     },
     {
         name: "Pasas",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/pasas.jpg",
         description: "Fruta deshidratada rica en fibra y antioxidantes.",
         price: 2.75,
         stock: 50
     },
     {
         name: "Jugo de naranja",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/jugo-naranja.jpg",
         description: "Jugo natural de naranjas frescas, rico en vitamina C y antioxidantes.",
         price: 2.50,
         stock: 30
     },
     {
         name: "Agua de coco",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/agua-coco.jpg",
         description: "Agua natural de coco, rica en electrolitos y nutrientes.",
         price: 3.00,
         stock: 25
     },
     {
         name: "Limonada natural",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/limonada.jpg",
         description: "Limones frescos exprimidos con agua y endulzados con miel natural.",
         price: 2.75,
         stock: 20
     },
     {
         name: "Té verde",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/te-verde.jpg",
         description: "Té verde natural, rico en antioxidantes y con propiedades antiinflamatorias.",
         price: 3.50,
         stock: 40
     },
     {
         name: "Jugo de manzana",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/jugo-manzana.jpg",
         description: "Jugo natural de manzanas frescas, rico en vitaminas y antioxidantes.",
         price: 2.25,
         stock: 30
     },
     {
         name: "Té de jengibre",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/te-jengibre.jpg",
         description: "Té natural de jengibre fresco, con propiedades antiinflamatorias y digestivas.",
         price: 3.25,
         stock: 20
     },
     {
         name: "Jugo de pomelo",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/jugo-pomelo.jpg",
         description: "Jugo natural de pomelo frescas, rico en vitamina C y antioxidantes.",
         price: 2.75,
         stock: 25
     },
     {
         name: "Leche de almendras",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/leche-almendras.jpg",
         description: "Leche vegetal de almendras, rica en grasas saludables y nutrientes.",
         price: 3.50,
         stock: 15
     },
     {
         name: "Té de menta",
-        image: "https://via.placeholder.com/150x150",
+        image: "https://biofresh.shop/img/te-menta.jpg",
         description: "Té natural de hojas de menta fresca, con propiedades digestivas y relajantes.",
         price: 2.50,
         stock: 30
