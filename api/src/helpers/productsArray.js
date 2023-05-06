8ee6fbb44ac76c3237d98e4af0cd75c51ecb68d1
santimuller45 <santiagomuller45@gmail.com>
2023-05-06 11:58:38 -0300

    products name and descrp modified

--- api/src/helpers/productsArray.js
+++ api/src/helpers/productsArray.js
@@ -1,134 +1,134 @@
 const productsArray = [
     {
-        name: "Almendras",
+        name: "Almonds",
         image: "https://biofresh.shop/img/almendras.jpg",
-        description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
+        description: "Nut rich in healthy fats, protein and fiber.",
         price: 2.50,
         stock: 50
     },
     {
-        name: "Dátiles",
+        name: "Dates",
         image: "https://biofresh.shop/img/datiles.jpg",
-        description: "Fruta deshidratada rica en fibra y vitaminas.",
+        description: "Dried fruit rich in fiber and vitamins.",
         price: 3.50,
         stock: 30
     },
     {
-        name: "Ciruelas pasas",
+        name: "Prunes",
         image: "https://biofresh.shop/img/pasas.jpg",
-        description: "Fruta deshidratada rica en fibra, antioxidantes y compuestos beneficiosos para la salud.",
+        description: "Dried fruit rich in fiber, antioxidants and beneficial compounds for health.",
         price: 4.00,
         stock: 20
     },
     {
-        name: "Nueces de Brasil",
+        name: "Brazil nuts",
         image: "https://biofresh.shop/img/nueces-brasil.jpg",
-        description: "Fruto seco rico en grasas saludables, proteínas y minerales.",
+        description: "Nut rich in healthy fats, proteins and minerals",
         price: 6.00,
         stock: 40
     },
     {
-        name: "Higos secos",
+        name: "Dried figs",
         image: "https://biofresh.shop/img/higos-secos.jpg",
-        description: "Fruta deshidratada rica en fibra, antioxidantes y compuestos beneficiosos para la salud.",
+        description: "Dried fruit rich in fiber, antioxidants and beneficial compounds for health.",
         price: 3.50,
         stock: 15
     },
     {
-        name: "Pistachos",
+        name: "Pistachios",
         image: "https://biofresh.shop/img/pistachios.jpg",
-        description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
+        description: "Nut rich in healthy fats, protein and fiber",
         price: 5.00,
         stock: 25
     },
     {
-        name: "Anacardos",
+        name: "Cashew nuts",
         image: "https://biofresh.shop/img/anacardos.jpg",
-        description: "Fruto seco rico en grasas saludables, proteínas y minerales.",
+        description: "Nut rich in healthy fats, proteins and minerals",
         price: 7.50,
         stock: 35
     },
     {
-        name: "Arándanos deshidratados",
+        name: "Dehydrated cranberries",
         image: "https://biofresh.shop/img/arandanos-desidratados.jpg",
-        description: "Fruta deshidratada rica en antioxidantes y compuestos beneficiosos para la salud.",
+        description: "Dried fruit rich in antioxidants and beneficial compounds for health",
         price: 4.25,
         stock: 10
     },
     {
-        name: "Avellanas",
+        name: "Hazelnuts",
         image: "https://biofresh.shop/img/avellanas.jpg",
-        description: "Fruto seco rico en grasas saludables, proteínas y fibra.",
+        description: "Nut rich in healthy fats, protein and fiber.",
         price: 2.25,
         stock: 60
     },
     {
-        name: "Pasas",
+        name: "Raisins",
         image: "https://biofresh.shop/img/pasas.jpg",
-        description: "Fruta deshidratada rica en fibra y antioxidantes.",
+        description: "Dried fruit rich in fiber and antioxidants",
         price: 2.75,
         stock: 50
     },
     {
-        name: "Jugo de naranja",
+        name: "Orange juice",
         image: "https://biofresh.shop/img/jugo-naranja.jpg",
-        description: "Jugo natural de naranjas frescas, rico en vitamina C y antioxidantes.",
+        description: "Natural juice of fresh oranges, rich in vitamin C and antioxidants",
         price: 2.50,
         stock: 30
     },
     {
-        name: "Agua de coco",
+        name: "Coconut water",
         image: "https://biofresh.shop/img/agua-coco.jpg",
-        description: "Agua natural de coco, rica en electrolitos y nutrientes.",
+        description: "Natural coconut water, rich in electrolytes and nutrients.",
         price: 3.00,
         stock: 25
     },
     {
-        name: "Limonada natural",
+        name: "Natural lemonade",
         image: "https://biofresh.shop/img/limonada.jpg",
-        description: "Limones frescos exprimidos con agua y endulzados con miel natural.",
+        description: "Fresh lemons squeezed with water and sweetened with natural honey.",
         price: 2.75,
         stock: 20
     },
     {
-        name: "Té verde",
+        name: "Green Tea",
         image: "https://biofresh.shop/img/te-verde.jpg",
-        description: "Té verde natural, rico en antioxidantes y con propiedades antiinflamatorias.",
+        description: "Natural green tea, rich in antioxidants and with anti-inflammatory properties.",
         price: 3.50,
         stock: 40
     },
     {
-        name: "Jugo de manzana",
+        name: "Apple juice",
         image: "https://biofresh.shop/img/jugo-manzana.jpg",
-        description: "Jugo natural de manzanas frescas, rico en vitaminas y antioxidantes.",
+        description: "Natural juice of fresh apples, rich in vitamins and antioxidants.",
         price: 2.25,
         stock: 30
     },
     {
-        name: "Té de jengibre",
+        name: "Ginger tea",
         image: "https://biofresh.shop/img/te-jengibre.jpg",
-        description: "Té natural de jengibre fresco, con propiedades antiinflamatorias y digestivas.",
+        description: "Natural fresh ginger tea, with anti-inflammatory and digestive properties.",
         price: 3.25,
         stock: 20
     },
     {
-        name: "Jugo de pomelo",
+        name: "Grapefruit juice",
         image: "https://biofresh.shop/img/jugo-pomelo.jpg",
-        description: "Jugo natural de pomelo frescas, rico en vitamina C y antioxidantes.",
+        description: "Natural fresh grapefruit juice, rich in vitamin C and antioxidants.",
         price: 2.75,
         stock: 25
     },
     {
-        name: "Leche de almendras",
+        name: "Almond milk",
         image: "https://biofresh.shop/img/leche-almendras.jpg",
-        description: "Leche vegetal de almendras, rica en grasas saludables y nutrientes.",
+        description: "Vegetable almond milk, rich in healthy fats and nutrients.",
         price: 3.50,
         stock: 15
     },
     {
-        name: "Té de menta",
+        name: "Mint tea",
         image: "https://biofresh.shop/img/te-de-menta.jpg",
-        description: "Té natural de hojas de menta fresca, con propiedades digestivas y relajantes.",
+        description: "Natural tea from fresh mint leaves, with digestive and relaxing properties.",
         price: 2.50,
         stock: 30
     }
