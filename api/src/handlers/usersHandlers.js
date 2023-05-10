ac1ed3184caafb82dddec4a43d76a1aea706028a
santimuller45 <santiagomuller45@gmail.com>
2023-05-10 19:54:13 -0300

    modificado los users de controllers y handlers no funcionales

--- api/src/handlers/usersHandlers.js
+++ api/src/handlers/usersHandlers.js
@@ -19,16 +19,6 @@ const getAllUsersHandler = async (req, res) => {
     }
 };
 
-// const getUserHandler = async (req, res) => {
-//     const { userEmail } = req.params;
-//     try {
-//         const user = await getUserDB(userEmail);
-//         res.status(200).json(user);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// }
-
 const loginUserHandler = async (req, res) => {
     const { email , password } = req.query;
     try {
