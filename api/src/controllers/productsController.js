dedcb91acc6623c5fd9ec21d32e05902449baf21
Mario Benitez <nadamasfacil.mb@gmail.com>
2023-05-12 08:37:12 -0600

    Product controller and product handler

--- api/src/controllers/productsController.js
+++ api/src/controllers/productsController.js
@@ -59,9 +59,44 @@ const getProductById = async(prodId) => {
     }]});
 };
 
-const getProductsByCategory = async (category) => {
-  const categoryFound = await Categories.findByPk(category);
-  const products = await categoryFound.getProducts();
+const getProductsByCategory = async (category, order) => {
+  let products = [];
+  let productsFilter = [];
+  let categoryFound = '';
+  if (category === 'All') {
+    products = await getAllProducts();
+  } else {
+    categoryFound = await Categories.findByPk(category);
+    if (categoryFound === null) return [];
+    products = await categoryFound.getProducts();
+  };
+  if (products.length === null) return [];
+  if (order) {
+    if (order === "AtoZ") productsFilter = products.sort((a,b) =>  a.name.localeCompare(b.name));
+    else if (order === "ZtoA") productsFilter = products.sort((a,b) => b.name.localeCompare(a.name));
+    else if (order === "Lower") {
+      console.log('entre a Lower');
+      productsFilter = products.sort((a,b) => 
+      {
+        if ( a.price > b.price ) return 1;
+        else if( a.price < b.price) return -1;
+        else return 0;
+      });
+    } 
+    else if (order === "Higher") {
+      console.log('entre a Higher');
+      productsFilter = products.sort((a,b) => 
+      {
+        if ( a.price < b.price) return 1;
+        else if ( a.price > b.price) return -1;
+        else return 0;
+      });
+    }
+    else {
+      console.log('igual');
+      productsFilter = products;
+    }
+  };
   return products;
 };
 
