6936dd8f44bd92fcfc9982546374d497b79ea975
santimuller45 <santiagomuller45@gmail.com>
2023-05-12 18:54:28 -0300

    eliminado del modelo user ID

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -4,12 +4,6 @@ module.exports = (sequelize) => {
   sequelize.define(
     "users",
     {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        unique: true,
-      },
       email: {
         type: DataTypes.STRING(100),
         primaryKey: true,
