fb86da67e085937d4ef0721bc49cc457d2106e23
santimuller45 <santiagomuller45@gmail.com>
2023-05-25 17:55:21 -0300

    mercadopago

--- api/src/handlers/paymentsHandlers.js
+++ api/src/handlers/paymentsHandlers.js
@@ -1,90 +1,62 @@
 const mercadopago = require("mercadopago");
 require('dotenv').config();
+const { MERCADOPAGO_KEY } = process.env;
 
-//////////////////////////////////////////////////////////////
-const paymentNotificationHandler = async (req, res) => {
-    try {
-let merchant_order = null;
+mercadopago.configurations.setAccessToken(MERCADOPAGO_KEY);
 
-switch (req.query.topic) {
-    case 'payment':
-        mercadopago.payment.findById(req.query.id)
-            .then(payment => {
-                merchant_order = mercadopago.merchantOrders.findById(payment.order.id);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        break;
-    case 'merchant_order':
-        merchant_order = mercadopago.merchantOrders.findById(req.query.id)
-            .catch(error => {
-                console.log(error);
-            });
-        break;
-}
-
-let paid_amount = 0;
-
-
-merchant_order.then(order => {
-    order.payments.forEach(payment => {
-        if (payment.status === 'approved') {
-            paid_amount += payment.transaction_amount;
-        }
-    });
-
-    if (paid_amount >= order.total_amount) {
-        if (order.shipments.length > 0) {
-            if (order.shipments[0].status === 'ready_to_ship') {
-                console.log('Totally paid. Print the label and release your item.');
-            }
-        } else {
-            console.log('Totally paid. Release your item.');
-        }
-    } else {
-        console.log('Not paid yet. Do not release your item.');
-    }
-
-})}
-catch (error)
-{
-    res.status(400).send({error: error.message});
-}};
-
-//////////////////////////////////////////////////////////////
 const paymentHandler = async (req, res) => {
     const { id , name , image , description , price } = req.body;
-    try {
-        let preference = {
-            items: [{
-                id: id,
-                title: name,
-                currency_id: "UYU",
-                picture_url: image,
-                description: description,
-                category_id: "articulo",
-                quantity: 1,
-                unit_price: price
-            }],
-            back_urls: {
-                success: "http://127.0.0.1:5173/",
-                failure: "http://127.0.0.1:5173/",
-                pending: "",
-            },
-            auto_return: "approved",
-            binary_mode: true,
-        }
-    mercadopago.preferences.create(preference)
-    .then((response) => res.status(200).send({response}))
-    .catch((error) => res.status(400).send({error}));
+    mercadopago.configure({
+      access_token: MERCADOPAGO_KEY,
+    });
 
-} catch (error) {
-    res.status(400).send({error:error.message});
-}
-};
+    try {
+      const result = await mercadopago.preferences.create({
+        items: [
+          {
+            id: id,
+            title: name,
+            unit_price: price,
+            currency_id: "UYU",
+            picture_url: image,
+            description: description,
+            quantity: 1,
+          },
+        ],
+        notification_url: "https://9f56-181-167-187-75.sa.ngrok.io/webhook",
+        back_urls: {
+          success: "http://localhost:3000/success",
+          pending: "",
+          failure: "",
+        },
+      });
+  
+      console.log(result);
+  
+      // res.json({ message: "Payment creted" });
+      res.json(result.body);
+    } catch (error) {
+      return res.status(500).json({ message: "Something goes wrong" });
+    }
+  };
+
+  const receiveWebhook = async (req, res) => {
+    try {
+      const payment = req.query;
+      console.log(payment);
+      if (payment.type === "payment") {
+        const data = await mercadopago.payment.findById(payment["data.id"]);
+        console.log(data);
+      }
+  
+      res.sendStatus(204);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Something goes wrong" });
+    }
+  };
 
 module.exports = {
     paymentHandler,
-    paymentNotificationHandler
+    receiveWebhook
 }
--- api/src/routes/paymentsRouter.js
+++ api/src/routes/paymentsRouter.js
@@ -3,11 +3,12 @@ const { Router } = require('express');
 // Requerir todos los handlers
 // ejemplo: const handlerProducts = require('../handlers/handlerProducts)
 
-const { paymentHandler, paymentNotificationHandler } = require('../handlers/paymentsHandlers');
+const { paymentHandler , receiveWebhook} = require('../handlers/paymentsHandlers');
 
 const paymentsRouter = Router();
 
 paymentsRouter.post('/', paymentHandler);
-paymentsRouter.post('/notifications', paymentNotificationHandler);
+paymentsRouter.post('/notifications', receiveWebhook)
+// paymentsRouter.post('/notifications', paymentNotificationHandler);
 
 module.exports = paymentsRouter;
